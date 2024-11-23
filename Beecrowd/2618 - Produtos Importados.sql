190eaa9ed5dccafb4528f2ecafa8d70200953efd
Victor Hugo Silva <90207063+VictorHugoSDev@users.noreply.github.com>
2024-11-23 14:30:30 -0300

    Create 2618 - Produtos Importados.sql

---
+++ Beecrowd/2618 - Produtos Importados.sql
@@ -0,0 +1,6 @@
+SELECT p.name, pr.name, c.name
+FROM products p
+JOIN providers pr ON pr.id = p.id_providers
+JOIN categories c ON c.id = p.id_categories
+WHERE pr.name = 'Sansul SA'
+AND c.name = 'Imported';
